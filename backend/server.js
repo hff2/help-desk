cc2ac1915247ed54c73a851d7439f6ed771448a7
hff2 <hophoebeho@gmail.com>
2022-06-17 14:50:49 +0800

    feat: basic express server setup

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,11 @@
-console.log('test');
+const express = require('express')
+const dotenv = require('dotenv').config()
+const port = process.env.PORT || 5000
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.status(200).json({ message: 'Welcome to the Help Desk API' })
+})
+
+app.listen(port, () => console.log(`Server started on port ${port}`))
