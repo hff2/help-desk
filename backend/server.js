5482a857f1bb860900e96485399c03a108f617da
hff2 <hophoebeho@gmail.com>
2022-06-17 19:48:52 +0800

    feat: connect to the database

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,13 @@
 const express = require('express')
+const colors = require('colors')
 const dotenv = require('dotenv').config()
 const { errorHandler } = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
 const PORT = process.env.PORT || 5000
 const app = express()
+
+// Connect to database
+connectDB()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
