bad44dda84cf8106ceeb70d2bd893a44bd935b6c
hff2 <hophoebeho@gmail.com>
2022-06-18 15:52:33 +0800

    feat: frontend folder setup

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Counter } from './features/counter/Counter';
+
+function App() {
+  return (
+    <div>
+      My app
+    </div>
+  );
+}
+
+export default App;
