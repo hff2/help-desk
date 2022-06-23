3a2abe95e239dba7b4401043381421fb74cd8032
hff2 <hophoebeho@gmail.com>
2022-06-23 19:22:44 +0800

    fix: fix the server crash problem

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ if (process.env.NODE_ENV === 'production') {
     // Set build folder as static
     app.use(express.static(path.join(__dirname, '../frontend/build')))
 
-    app.get('*', (req, res) => res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html'))
+    app.get('*', (req, res) => res.sendFile(path.join(__dirname, '../frontend/build/index.html')))
 }
 else {
     app.get('/', (req, res) => {
